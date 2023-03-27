a3f110ee93b6ae0d34ccb176dbb7a1860b41bf53
Mohammadreza Babayi <m.babayi574@gmail.com>
2023-03-27 17:24:44 +0430

    clean project

--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./footer.module.css";
 import Image from "next/Image";
 
 import { Box, Flex, Link, Text, Divider } from "@chakra-ui/react";
@@ -50,13 +49,13 @@ const Footer = () => {
         {/* Container with 50% width in center */}
         <div
           style={{
-            width: "60%",
+            width: "60vw",
             margin: "0 auto",
-            padding: "1rem",
+            padding: "1vw",
           }}
         >
           <Flex direction="row" justify="space-between" align="center">
-            <Box p="4">
+            <Box p="7">
               {/* Heading "معرفی تیم‎ها"
                   after that, we have two columns with eight links in each column */}
               <Text
@@ -89,11 +88,11 @@ const Footer = () => {
               </Flex>
             </Box>
 
-            <Box p="4">
+            <Box p="5">
               {/* Google Map with company location */}
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3238.5167629636476!2d51.44108551551574!3d35.73810133445774!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f8e0250c928f8d9%3A0x4c88d8c2a938935a!2skoodakancharity!5e0!3m2!1sen!2s!4v1679211434516!5m2!1sen!2s"
-                width="500"
+                maxWidth="800vw"
                 // height="450"
                 style={{ border: 0, borderRadius: "10px" }}
                 allowfullscreen=""
@@ -107,31 +106,39 @@ const Footer = () => {
               ما را در شبکه‎های اجتماعی دنبال کنید
             </Text>
 
-            <IconLink>
-              <Image src={TelegramIcon} alt="Telegram" />
-            </IconLink>
-            <IconLink>
-              <Image src={WhatsappIcon} alt="Whatsapp" />
-            </IconLink>
-            <IconLink>
-              <Image src={AparatIcon} alt="Aparat" />
-            </IconLink>
-            <IconLink>
-              <Image src={YoutubeIcon} alt="Youtube" />
-            </IconLink>
-            <IconLink>
-              <Image src={LinkedinIcon} alt="Linkedin" />
-            </IconLink>
-            <IconLink>
-              <Image src={TwitterIcon} alt="Twitter" />
-            </IconLink>
-            <IconLink>
-              <Image src={InstagramIcon} alt="Instagram" />
-            </IconLink>
-            <div></div>
+            <div
+              style={{
+                display: "flex",
+              }}
+            >
+              <IconLink>
+                <Image src={TelegramIcon} alt="Telegram" />
+              </IconLink>
+              <IconLink>
+                <Image src={WhatsappIcon} alt="Whatsapp" />
+              </IconLink>
+              <IconLink>
+                <Image src={AparatIcon} alt="Aparat" />
+              </IconLink>
+              <IconLink>
+                <Image src={YoutubeIcon} alt="Youtube" />
+              </IconLink>
+              <IconLink>
+                <Image src={LinkedinIcon} alt="Linkedin" />
+              </IconLink>
+              <IconLink>
+                <Image src={TwitterIcon} alt="Twitter" />
+              </IconLink>
+              <IconLink>
+                <Image src={InstagramIcon} alt="Instagram" />
+              </IconLink>
+            </div>
           </Flex>
-          <br />
-          <Divider borderColor="white" border="1px" color="white" />
+
+          <div>
+            <br />
+            <Divider borderColor="white" border="1px" color="white" />
+          </div>
 
           <Flex
             direction="row"

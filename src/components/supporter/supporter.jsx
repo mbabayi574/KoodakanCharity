344b41b5103e9ba950bfbc60091fa3f4e4a75eb8
sadegh <ghasemnejad.sadegh@gmail.com>
2023-03-28 14:40:04 +0330

    finsih supporter section

--- src/components/supporter/supporter.jsx
+++ src/components/supporter/supporter.jsx
@@ -1,12 +1,115 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Circle } from "@chakra-ui/react";
+import { ChevronRightIcon, ChevronLeftIcon } from "@chakra-ui/icons";
 import TitleBox from "../shared/title-box";
-const Supporter = () => {
+import Image from "next/legacy/image";
+import lohImage from "@/assets/images/logo.png";
+import digikala from "@/assets/images/company/digikala.png";
+import burgerland from "@/assets/images/company/burger-land.png";
+import rozhine from "@/assets/images/company/rozhine.png";
+import { useState } from "react";
+const Slider = ({ images }) => {
+  const [curSlide, setCurSlide] = useState(0);
+  const nextPageHandler = () => {
+    if (curSlide > Math.floor(images.length / 2) - 1) {
+      setCurSlide(0);
+    } else {
+      setCurSlide(curSlide + 1);
+    }
+  };
+  const lastPageHandler = () => {
+    if (curSlide < 1) {
+      setCurSlide(Math.floor(images.length / 2));
+    } else {
+      setCurSlide(curSlide - 1);
+    }
+  };
   return (
-    <Flex>
-      <Flex>
+    <Flex
+      className="supporter-slider"
+      justifyContent={"space-between"}
+      alignItems="center"
+    >
+      <Circle
+        size="48px"
+        bg="white"
+        color="primary"
+        onClick={nextPageHandler}
+        cursor="pointer"
+      >
+        <ChevronRightIcon w={8} h={8} />
+      </Circle>
+      <div className="supporter-slider__images">
+        {images.map((image) => (
+          <>
+            <div
+              key={image.id}
+              className="supporter-slider__images__image"
+              style={{ transform: `translateX(-${curSlide * 110}%)` }}
+            >
+              <Image
+                src={image.src}
+                alt={image.alt}
+                layout="intrinsic"
+                // objectFit="cover"
+              />
+            </div>
+          </>
+        ))}
+      </div>
+      <Circle
+        size="48px"
+        bg="white"
+        color="primary"
+        onClick={lastPageHandler}
+        cursor="pointer"
+      >
+        <ChevronLeftIcon w={8} h={8} />
+      </Circle>
+    </Flex>
+  );
+};
+const Supporter = () => {
+  const lohImagesData = [
+    { id: 0, src: lohImage, alt: "loh" },
+    { id: 1, src: lohImage, alt: "loh" },
+    { id: 2, src: lohImage, alt: "loh" },
+    { id: 3, src: lohImage, alt: "loh" },
+    { id: 4, src: lohImage, alt: "loh" },
+    { id: 45, src: lohImage, alt: "loh" },
+  ];
+  const companyImagesData = [
+    { id: 0, src: digikala, alt: "loh" },
+    { id: 1, src: burgerland, alt: "loh" },
+    { id: 2, src: rozhine, alt: "loh" },
+    { id: 3, src: digikala, alt: "loh" },
+    { id: 4, src: burgerland, alt: "loh" },
+    { id: 45, src: rozhine, alt: "loh" },
+  ];
+  return (
+    <Flex
+      justifyContent={"space-between"}
+      paddingX="56px"
+      alignItems={"center"}
+    >
+      <Flex
+        direction={"column"}
+        flex="0 0 40%"
+        gap="56px"
+        alignItems={"center"}
+      >
         <TitleBox text="لوح های کودکان فرشته اند" />
+        <Slider images={lohImagesData} />
       </Flex>
-      <Flex></Flex>
+      <div className="supporter-seprator"></div>
+      <Flex
+        direction={"column"}
+        flex="0 0 40%"
+        gap="56px"
+        alignItems={"center"}
+      >
+        <TitleBox text="حامیان ما" />
+        <Slider images={companyImagesData} />
+      </Flex>
     </Flex>
   );
 };
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,7 @@ import { RtlProvider } from "@/components/rtl-provider";
 import Header from "@/components/shared/header/Header";
 import "@/components/hero/hero.scss";
 import "@/styles/shared/title-box.scss";
+import "@/components/supporter/supporter.scss";
 import "@/styles/global.scss";
 // 1. Import `extendTheme`
 import { extendTheme } from "@chakra-ui/react";
@@ -10,6 +11,7 @@ import { extendTheme } from "@chakra-ui/react";
 // 2. Call `extendTheme` and pass your custom values
 const theme = extendTheme({
   colors: {
+    primary: "#236870",
     brand: {
       100: "#f7fafc",
       900: "#1a202c",
